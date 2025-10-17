3d37f2ce1d0535b8442e1dcca5728a47c024478d
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2025-10-17 19:24:30 +0200

    initialize sizes in EigenMatrixTypes.h

--- Original/EigenMatrixTypes.h
+++ Original/EigenMatrixTypes.h
@@ -1,12 +1,16 @@
 //#include <Eigen/Dense>
 #include </usr/include/eigen3/Eigen/Dense>
 
-typedef Eigen::Matrix<double,10,1> SampleVector;
-typedef Eigen::Matrix<double,19,1> FullSampleVector;
-typedef Eigen::Matrix<double,Eigen::Dynamic,1,0,10,1> PulseVector;
-typedef Eigen::Matrix<char,Eigen::Dynamic,1,0,10,1> BXVector;
-typedef Eigen::Matrix<double,10,10> SampleMatrix;
-typedef Eigen::Matrix<double,19,19> FullSampleMatrix;
-typedef Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,0,10,10> PulseMatrix;
-typedef Eigen::Matrix<double,10,Eigen::Dynamic,0,10,10> SamplePulseMatrix;
+int SampleVectorSize = 10;
+int FullSampleVectorSize = 19;
+int PulseVectorSize = 12;
+
+typedef Eigen::Matrix<double,SampleVectorSize,1> SampleVector;
+typedef Eigen::Matrix<double,FullSampleVectorSize,1> FullSampleVector;
+typedef Eigen::Matrix<double,Eigen::Dynamic,1,0,PulseVectorSize,1> PulseVector;
+typedef Eigen::Matrix<char,Eigen::Dynamic,1,0,PulseVectorSize,1> BXVector;
+typedef Eigen::Matrix<double,PulseVectorSize,PulseVectorSize> SampleMatrix;
+typedef Eigen::Matrix<double,FullSampleVectorSize,FullSampleVectorSize> FullSampleMatrix;
+typedef Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,0,PulseVectorSize,PulseVectorSize> PulseMatrix;
+typedef Eigen::Matrix<double,PulseVectorSize,Eigen::Dynamic,0,PulseVectorSize,PulseVectorSize> SamplePulseMatrix;
 typedef Eigen::LLT<SampleMatrix> SampleDecompLLT;
