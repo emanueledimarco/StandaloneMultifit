b256ad7815d8d39a0af0387d450477b7929bf6a9
Ruben Gargiulo <ruben@membri.com>
2026-02-09 10:55:27 +0100

    gen parallized (to test)

---
+++ Original/gen.sh
@@ -0,0 +1,1 @@
+for i in $(seq 0 10); do bash -c "./CreateData.x 0 1000 16 6.25 200 200 1 1 TestBeamPhase2 0 0.5 0.5 0 1 $RANDOM&"; done
--- Original/run.sh
+++ Original/run.sh
@@ -1,7 +1,8 @@
-#./CreateData.x 0 1000 16 6.25 200 10 1 1 TestBeamPhase2 0 0.5 0.5 0 1
-
-input="inputExternal/mysample_1000_0.000_0.000_16_6.25_10.00_200.00_1.000_1.00_TestBeamPhase2_0.50_slew_0.00_FlatEnergy.root"
+#input="inputExternal/mysample_1000_0.000_0.000_16_6.25_10.00_200.00_1.000_1.00_TestBeamPhase2_0.50_slew_0.00_FlatEnergy.root"
 #input="inputExternal/mysample_11_0.000_0.000_16_6.25_10.00_200.00_1.000_1.00_TestBeamPhase2_0.50_slew_0.00_FlatEnergy.root"
+
+
+input="inputExternal/mysample_10_0.000_0.000_16_6.25_200.00_200.00_1.000_1.00_TestBeamPhase2_0.50_slew_0.00_FlatEnergy_cat.root"
 
 ./multifit.x $input 1 1
 
